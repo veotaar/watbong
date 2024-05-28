7d864f558fe9f3223ed54ddf63cfb04fbad1e5f7
Yalçıncan Ulus <41164799+veotaar@users.noreply.github.com>
2024-05-28 13:07:16 +0300

    add categories

---
+++ src/data/categories.ts
@@ -0,0 +1,19 @@
+const categories = {
+  BURGERS: 'burgers',
+  SANDWICHES: 'sandwiches',
+  MACANDCHEESE: 'mac-and-cheese',
+  MAINS: 'mains',
+  SNACKS: 'snacks',
+  SOUPS: 'soups',
+  DESSERTS: 'desserts',
+  SOFTDRINKS: 'soft-drinks',
+  JUICES: 'juices',
+  SHAKES: 'shakes',
+  COFFEETEA: 'coffee-tea',
+  SPIRITS: 'spirits',
+  WINES: 'wines',
+  BEERS: 'beers',
+  COCKTAILS: 'cocktails',
+};
+
+export default categories;
